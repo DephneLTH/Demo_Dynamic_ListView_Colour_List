841ec4c00f00617dc74aee02905ba549857b5b8b
22035660 <22035660@myrp.edu.sg>
2023-06-05 20:04:37 +0800

    Initial commit

---
+++ app/src/main/java/sg/edu/rp/c346/id22035660/demodynamiclistviewcolourlist/MainActivity.java
@@ -0,0 +1,71 @@
+package sg.edu.rp.c346.id22035660.demodynamiclistviewcolourlist;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Adapter;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.Toast;
+
+import java.util.ArrayList;
+
+public class MainActivity extends AppCompatActivity {
+
+    EditText etElement;
+    Button btnAdd;
+    ListView lvColour;
+    ArrayList<String> alColour;
+    ArrayAdapter<String> aaColour;
+    EditText etIndexElement;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        etElement = findViewById(R.id.editTextColour);
+        btnAdd = findViewById(R.id.buttonAddItem);
+        lvColour = findViewById(R.id.listViewColour);
+        etIndexElement = findViewById(R.id.editTextIndex);
+
+        alColour = new ArrayList<>();
+        alColour.add ("Red");
+        alColour.add ("Orange");
+
+        aaColour = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, alColour);
+
+        lvColour.setAdapter(aaColour);
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String colour = etElement.getText().toString();
+                int pos = Integer.parseInt(etIndexElement.getText().toString());
+
+                if (pos >= 0 && pos <= alColour.size()) {
+                    alColour.add(pos, colour);
+                    aaColour.notifyDataSetChanged();
+                } else {
+                    Toast.makeText(MainActivity.this, "Invalid position", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
+        lvColour.setOnItemClickListener(new AdapterView.OnItemClickListener(){
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                        String colour = alColour.get(position);
+                        Toast.makeText(getApplicationContext(), colour, Toast.LENGTH_SHORT).show();
+                    }
+                });
+            }
+        }
+
+
+
+
